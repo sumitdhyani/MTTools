18121b0dc4283094258fe1f2e7a0f9deba24e3e7
sumitdhyani <sumitdhyani1@gmail.com>
2025-11-11 15:14:20 +0530

    Used concrete objects instead of unique_ptrs and shared_ptrs

--- TaskThrottlers.hpp
+++ TaskThrottlers.hpp
@@ -7,54 +7,47 @@ namespace ULMTTools
 	class ThrottledWorkerThread
 	{
 		typedef mtInternalUtils::ThrottledConsumerThread<Task> ThrottledConsumerThread;
-		DEFINE_UNIQUE_PTR(ThrottledConsumerThread)
-
-			ThrottledConsumerThread_UPtr m_consumer;
+		ThrottledConsumerThread m_consumer;
 
 
 	public:
-#if defined(ENABLE_MTTOOLS_TESTING)//This constructor is for testing purpose only, not available to the client code
-		ThrottledWorkerThread(std::shared_ptr<std::vector<Task>> queue, duration unitTime, size_t numTransactions, std::function<void(Task)> executor = [](Task task) {task(); })
-			:m_consumer(std::make_unique<ThrottledConsumerThread>(queue, executor, unitTime, numTransactions))
-		{
-		}
-#endif //ENABLE_MTTOOLS_TESTSING
 
 		ThrottledWorkerThread(const duration& unitTime, const size_t& numTransactions)
-			:m_consumer(std::make_unique<ThrottledConsumerThread>(std::make_shared<std::vector<Task>>(), [](Task task) {task(); }, unitTime, numTransactions))
+			:m_consumer([](Task task) {task(); }, unitTime, numTransactions)
 		{
 		}
 
 		void push(const Task& task)
 		{
-			m_consumer->push(task);
+			m_consumer.push(task);
 		}
 
 		void kill()
 		{
-			m_consumer->kill();
+			m_consumer.kill();
 		}
 	};
 
 	class ReusableThrottledWorkerThread
 	{
 		typedef mtInternalUtils::ReusableThrottler<Task> ReusableThrottler;
-		DEFINE_UNIQUE_PTR(ReusableThrottler)
-
-			ReusableThrottler_UPtr m_consumer;
+		ReusableThrottler m_throttler;
 	public:
 		ReusableThrottledWorkerThread(const std::shared_ptr<WorkerThread>& worker,
 																	const std::shared_ptr<TaskScheduler> taskScheduler,
 																	const duration& unitTime,
 																	const size_t& numTransactions)
-			:m_consumer(std::make_unique<ReusableThrottler>(worker, taskScheduler, [](Task task) {task(); }, unitTime, numTransactions))
+			:m_throttler([worker](const Task& task) { worker->push(task); },
+										[taskScheduler](const time_point& scheduleTime, const Task& task) { taskScheduler->push(scheduleTime, task); },
+										[](Task task) { task(); },
+										unitTime,
+										numTransactions)
 		{
 		}
 
 		void push(const Task& task)
 		{
-			m_consumer->push(task);
+			m_throttler.push(task);
 		}
 	};
-	DEFINE_PTR(ReusableThrottledWorkerThread)
 }
